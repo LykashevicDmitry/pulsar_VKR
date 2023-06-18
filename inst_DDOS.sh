8dcd5f3b8eed6437d44b6ae60efbfa803cfe7230
LykashevicDmitry <74460099+LykashevicDmitry@users.noreply.github.com>
2023-06-18 17:35:32 +0500

    baza

---
+++ inst_DDOS.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt -y update && apt -y install curl wget libcurl4 libssl-dev python3 python3-pip make cmake automake autoconf m4 build-essential git && git clone https://github.com/MatrixTM/MHDDoS.git && cd MH* && pip3 install -r requirements.txt
---
+++ monitoring.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+    curl http://example.com # замените example.com на нужный домен
+    sleep 10 # задержка в 10 секунд перед следующим запросом
+done
